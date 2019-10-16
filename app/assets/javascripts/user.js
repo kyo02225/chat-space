3a75d8da83dc0f3e38bb73a0c56fae01f4d118bf
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-10-16 20:04:17 +0900

    デッバガー削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,5 @@
 $(function() {
   $(".chat-group-form__input").on("keyup", function() {
-    var input = $(".chat-group-form__input").val();
-  console.log(input);
+    var input = $(".chat-group-form__input").val();  
   });
 });
