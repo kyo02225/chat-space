e1a8c82cb951d1898fc04f4b3e5d957650d717ed
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-10-17 15:18:29 +0900

    ユーザー追加リスト、ユーザー追加ボタン機能、ユーザー削除ボタン機能を追加

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -32,13 +32,35 @@ $(function() {
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
-          console.log(user)
         });
       }
       else {
         appendNoUser("一致する名前は有りません");
       }
     })
+    .fail(function() {
+      alert('ユーザーの検索に失敗しました');
+    })
+  
+    function addUser(user_id, user_name) {
+			var html = `<div class='chat-group-user clearfix' id='chat-group-user-8>
+										<input name='group[user_ids][]' type='hidden' value='${user_id}'>
+											<p class='chat-group-user__name'>${user_name}</p>
+											<a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+									</div>`
+			$('#chat-group-users').append(html);
+		}
+  
+    $('#user-search-result').on('click','.chat-group-user__btn--add',function(){
+			var user_id = $(this).data('user-id');
+			var user_name = $(this).data('user-name');
+			addUser(user_id, user_name);
+			$(this).parent().remove();
+		});
+  
+    $('#chat-group-users').on('click','.chat-group-user__btn--remove',function(){
+			$(this).parent().remove();
+		});
   });
 });
 
