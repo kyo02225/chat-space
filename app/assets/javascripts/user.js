fa412d0d4cda70a64a94f4dcf646a09ee6756983
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-10-18 11:52:04 +0900

    error修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -43,10 +43,10 @@ $(function() {
     })
   
     function addUser(user_id, user_name) {
-			var html = `<div class='chat-group-user clearfix' id='chat-group-user-8>
+			var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user_id}>
 										<input name='group[user_ids][]' type='hidden' value='${user_id}'>
 											<p class='chat-group-user__name'>${user_name}</p>
-											<a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+											<a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id='${user_id}' data-user-name='${user_name}'>削除</a>
 									</div>`
 			$('#chat-group-users').append(html);
 		}
